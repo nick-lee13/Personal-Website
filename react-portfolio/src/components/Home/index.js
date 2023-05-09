574ab70115a2d796d92f00a665cc06ada1f51537
nick-lee13 <ntlee@mta.ca>
2023-05-08 22:11:09 -0300

    Home page

---
+++ react-portfolio/src/components/Home/index.js
@@ -0,0 +1,44 @@
+import { Link } from 'react-router-dom';
+import LogoTitle from '../../assets/images/logo-s.png';
+import './index.scss';
+import { useEffect, useState } from 'react';
+import AnimatedLetters from '../AnimatedLetters';
+import Logo from './Logo';
+
+const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['N','i','c','k',' ','L','e','e']
+    const jobArray = ['D','e','v','e','l','o','p','e','r']
+
+    useEffect(() => {
+        let timeoutId = setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 4000)
+        return () => {
+            clearTimeout(timeoutId)
+        }
+    }, [])
+
+    return (
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>
+                <span className={letterClass}>H</span>
+                <span className={`${letterClass} _12`}>i,</span>
+                <br />
+                <span className={`${letterClass} _13`}>I</span>
+                <span className={`${letterClass} _14`}>'m</span>
+                <img src={LogoTitle} alt="developer" />
+                <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15} />
+                <br />
+                <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={23} />
+                </h1>
+                <h2>Developer / Innovator / Problem-Solver</h2>
+                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+            </div>
+            <Logo />
+        </div>
+    );
+}
+
+export default Home
