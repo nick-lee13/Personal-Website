356d0a2522a24097b2c874466713512346eaf439
nick-lee13 <ntlee@mta.ca>
2023-05-09 01:00:52 -0300

    added map to contact page

--- react-portfolio/src/components/Contact/index.js
+++ react-portfolio/src/components/Contact/index.js
@@ -3,6 +3,7 @@ import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
 import { useEffect, useRef, useState } from 'react'
 import emailjs from '@emailjs/browser'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 
 const Contact = () => {
 
@@ -71,6 +72,22 @@ const Contact = () => {
                         </ul>
                     </form>
                 </div>
+            </div>
+            <div className='info-map'>
+                Nick Lee
+                <br />
+                Halifax,
+                <br />
+                NS, Canada <br />
+                <span>nicklee.tech@gmail.com</span>
+            </div>
+            <div className='map-wrap'>
+                <MapContainer center={[44.647130875432126, -63.580263880602175]} zoom={13}>
+                    <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                    <Marker position={[44.647130875432126,-63.580263880602175]}>
+                        <Popup>Nick lives here!</Popup>
+                    </Marker>
+                </MapContainer>
             </div>
         </div>
         <Loader type="pacman" />
